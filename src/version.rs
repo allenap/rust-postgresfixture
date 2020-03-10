88f7dc886cffcec1184e9e8b0ec41f61b04fca3d
Gavin Panella <gavinpanella@gmail.com>
2020-03-10 18:45:18 +0100

    Test the version parser.

--- src/version.rs
+++ src/version.rs
@@ -3,14 +3,14 @@ use std::{error, fmt, io, num};
 
 use regex::Regex;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Version {
     pub major: u32,
     pub minor: u32,
     pub patch: Option<u32>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum VersionParseError {
     Invalid,
     Missing,
@@ -45,7 +45,7 @@ impl FromStr for Version {
     type Err = VersionParseError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let re = Regex::new(r"(?x) \b (\d+) [.] (\d+) (?: [.] (\d+) ) \b").unwrap();
+        let re = Regex::new(r"(?x) \b (\d+) [.] (\d+) (?: [.] (\d+) )? \b").unwrap();
         match re.captures(s) {
             Some(caps) => Ok(Version {
                 major: caps[1].parse()?,
@@ -100,5 +100,47 @@ impl From<io::Error> for VersionError {
 impl From<VersionParseError> for VersionError {
     fn from(error: VersionParseError) -> VersionError {
         VersionError::Invalid(error)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Version, VersionParseError::*};
+
+    #[test]
+    fn parses_version_below_10() {
+        assert_eq!(Ok(vp(9, 6, 17)), "9.6.17".parse());
+    }
+
+    #[test]
+    fn parses_version_above_10() {
+        assert_eq!(Ok(v(12, 2)), "12.2".parse());
+    }
+
+    #[test]
+    fn parse_returns_error_when_version_is_invalid() {
+        // 4294967295 is (2^32 + 1), so won't fit in a u32.
+        assert_eq!(Err(Invalid), "4294967296.0".parse::<Version>());
+    }
+
+    #[test]
+    fn parse_returns_error_when_version_not_found() {
+        assert_eq!(Err(Missing), "foo".parse::<Version>());
+    }
+
+    fn vp(major: u32, minor: u32, patch: u32) -> Version {
+        Version {
+            major: major,
+            minor: minor,
+            patch: Some(patch),
+        }
+    }
+
+    fn v(major: u32, minor: u32) -> Version {
+        Version {
+            major: major,
+            minor: minor,
+            patch: None,
+        }
     }
 }
