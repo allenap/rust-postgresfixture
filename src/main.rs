403853fa87fdb9a0e8ec0bd667dfa8cb46c36b5f
Gavin Panella <gavinpanella@gmail.com>
2021-10-30 11:15:32 +0200

    Improve command-line argument parsing and help output

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,7 @@ fn main() {
     exit(match parse_args().subcommand() {
         ("shell", Some(args)) => {
             let database_dir: PathBuf = args.value_of("database-dir").unwrap().into();
-            let database_name = match args.value_of("database-name") {
-                Some(name) => name.into(),
-                None => match env::var("PGDATABASE") {
-                    Ok(name) => name,
-                    Err(_) => "data".into(),
-                },
-            };
+            let database_name: String = args.value_of("database-name").unwrap().into();
             shell(database_dir, &database_name)
         }
         (command, _) => {
@@ -43,19 +37,25 @@ fn parse_args() -> clap::ArgMatches<'static> {
                 .about("Start a psql shell, creating and starting the cluster as necessary.")
                 .arg(
                     Arg::with_name("database-dir")
-                        .help("The directory in which to place, or find, the cluster.")
+                        .help(concat!(
+                            "The directory in which to place, or find, the cluster. The default ",
+                            "is taken from the PGDATA environment variable. If that is not set, ",
+                            "the cluster will be created in a directory named 'cluster'."))
                         .short("D")
                         .long("datadir")
-                        .value_name("DIR")
-                        .default_value("db"),
+                        .value_name("PGDATA")
+                        .default_value("cluster")
+                        .env("PGDATA"),
                 )
                 .arg(
                     Arg::with_name("database-name")
                         .help(concat!(
-                    "The database to connect to. The default is taken from the PGDATABASE ",
-                    "environment variable. If that is not set, a database named 'data' will ",
-                    "be created."))
-                        .value_name("PGDATABASE"),
+                            "The database to connect to. The default is taken from the PGDATABASE ",
+                            "environment variable. If that is not set, a database named 'data' will ",
+                            "be created."))
+                        .value_name("PGDATABASE")
+                        .default_value("data")
+                        .env("PGDATABASE"),
                 ),
         )
         .get_matches()
@@ -64,18 +64,22 @@ fn parse_args() -> clap::ArgMatches<'static> {
 fn shell(database_dir: PathBuf, database_name: &str) -> i32 {
     let cluster = postgresfixture::Cluster::new(
         match database_dir.is_absolute() {
-            false => env::current_dir().unwrap().join(database_dir),
             true => database_dir,
+            false => env::current_dir()
+                .expect("could not get current working directory")
+                .join(database_dir),
         },
         postgresfixture::Runtime::default(),
     );
     cluster.start().expect("could not start cluster");
     if !cluster
         .databases()
-        .unwrap()
+        .expect("could not list databases")
         .contains(&database_name.to_string())
     {
-        cluster.createdb(database_name).unwrap();
+        cluster
+            .createdb(database_name)
+            .expect(&format!("could not create database {:?}", database_name));
     }
     cluster.shell(&database_name).expect("shell failed");
     cluster.stop().expect("could not stop cluster");
