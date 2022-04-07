2b03842c686300c6d706a883544378332a3af46f
Gavin Panella <gavinpanella@gmail.com>
2022-04-07 13:26:47 +0200

    Add `exec` subcommand to CLI

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod cli;
 
+use std::ffi::OsStr;
 use std::fs;
 use std::io;
 use std::path::PathBuf;
@@ -12,10 +13,12 @@ use postgresfixture::{cluster, coordinate, lock, runtime};
 fn main() {
     let cli = cli::Cli::parse();
     exit(match cli.command {
-        cli::Commands::Shell {
-            database_dir,
-            database_name,
-        } => shell(database_dir, &database_name),
+        cli::Commands::Shell(database) => shell(database.dir, &database.name),
+        cli::Commands::Exec {
+            database,
+            command,
+            args,
+        } => exec(database.dir, &database.name, command, &args),
     });
 }
 
@@ -53,6 +56,51 @@ fn shell(database_dir: PathBuf, database_name: &str) -> i32 {
                 .expect("could not create database");
         }
         cluster.shell(database_name).expect("shell failed");
+    })
+    .unwrap();
+
+    0
+}
+
+fn exec<T: AsRef<OsStr>>(
+    database_dir: PathBuf,
+    database_name: &str,
+    command: T,
+    args: &[T],
+) -> i32 {
+    // Create the cluster directory first.
+    match fs::create_dir(&database_dir) {
+        Err(err) if err.kind() == io::ErrorKind::AlreadyExists => (),
+        other => other.expect("could not create database directory"),
+    };
+
+    // Obtain a canonical path to the cluster directory.
+    let database_dir = database_dir
+        .canonicalize()
+        .expect("could not canonicalize database directory");
+
+    // Use the canonical path to construct the UUID with which we'll lock this
+    // cluster. Use the `Debug` form of `database_dir` for the lock file UUID.
+    let lock_uuid = uuid::Uuid::new_v5(&UUID_NS, format!("{:?}", &database_dir).as_bytes());
+    let lock = lock::UnlockedFile::try_from(&lock_uuid).expect("could not create lock file");
+
+    // For now use the default PostgreSQL runtime.
+    let runtime = runtime::Runtime::default();
+    let cluster = cluster::Cluster::new(&database_dir, runtime);
+
+    coordinate::run_and_stop(&cluster, lock, || {
+        if !cluster
+            .databases()
+            .expect("could not list databases")
+            .contains(&database_name.to_string())
+        {
+            cluster
+                .createdb(database_name)
+                .expect("could not create database");
+        }
+        cluster
+            .exec(database_name, command, args)
+            .expect("shell failed");
     })
     .unwrap();
 
