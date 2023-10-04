cfc122e2e1b85e78e710080fbd7eddea7a7276cd
Gavin Panella <gavin@allenap.me>
2023-10-04 13:07:12 +0200

    Rename `PartialVersion`'s variants

--- src/version/partial.rs
+++ src/version/partial.rs
@@ -8,9 +8,12 @@ use super::VersionError;
 
 #[derive(Copy, Clone, Debug)]
 pub enum PartialVersion {
-    A(u32, u32, u32),
-    B(u32, u32),
-    C(u32),
+    /// Major, Minor, Patch.
+    Mmp(u32, u32, u32),
+    /// Major, Minor.
+    Mm(u32, u32),
+    /// Major.
+    M(u32),
 }
 
 impl PartialVersion {
@@ -23,10 +26,10 @@ impl PartialVersion {
     #[allow(dead_code)]
     pub fn sort_key(&self) -> (u32, Option<u32>, Option<u32>) {
         use PartialVersion::*;
-        match self {
-            A(a, b, c) => (*a, Some(*b), Some(*c)),
-            B(a, b) => (*a, Some(*b), None),
-            C(a) => (*a, None, None),
+        match *self {
+            Mmp(a, b, c) => (a, Some(b), Some(c)),
+            Mm(a, b) => (a, Some(b), None),
+            M(a) => (a, None, None),
         }
     }
 }
@@ -41,15 +44,15 @@ impl PartialOrd for PartialVersion {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         use PartialVersion::*;
         match (*self, *other) {
-            (A(x1, x2, x3), A(y1, y2, y3)) => (x1, x2, x3).partial_cmp(&(y1, y2, y3)),
-            (A(x1, x2, x3), B(y1, y2)) => (x1, x2, x3).partial_cmp(&(y1, y2, 0)),
-            (A(x1, x2, x3), C(y1)) => (x1, x2, x3).partial_cmp(&(y1, 0, 0)),
-            (B(x1, x2), A(y1, y2, y3)) => (x1, x2, 0).partial_cmp(&(y1, y2, y3)),
-            (B(x1, x2), B(y1, y2)) => (x1, x2).partial_cmp(&(y1, y2)),
-            (B(x1, x2), C(y1)) => (x1, x2).partial_cmp(&(y1, 0)),
-            (C(x1), A(y1, y2, y3)) => (x1, 0, 0).partial_cmp(&(y1, y2, y3)),
-            (C(x1), B(y1, y2)) => (x1, 0).partial_cmp(&(y1, y2)),
-            (C(x1), C(y1)) => x1.partial_cmp(&y1),
+            (Mmp(x1, x2, x3), Mmp(y1, y2, y3)) => (x1, x2, x3).partial_cmp(&(y1, y2, y3)),
+            (Mmp(x1, x2, x3), Mm(y1, y2)) => (x1, x2, x3).partial_cmp(&(y1, y2, 0)),
+            (Mmp(x1, x2, x3), M(y1)) => (x1, x2, x3).partial_cmp(&(y1, 0, 0)),
+            (Mm(x1, x2), Mmp(y1, y2, y3)) => (x1, x2, 0).partial_cmp(&(y1, y2, y3)),
+            (Mm(x1, x2), Mm(y1, y2)) => (x1, x2).partial_cmp(&(y1, y2)),
+            (Mm(x1, x2), M(y1)) => (x1, x2).partial_cmp(&(y1, 0)),
+            (M(x1), Mmp(y1, y2, y3)) => (x1, 0, 0).partial_cmp(&(y1, y2, y3)),
+            (M(x1), Mm(y1, y2)) => (x1, 0).partial_cmp(&(y1, y2)),
+            (M(x1), M(y1)) => x1.partial_cmp(&y1),
         }
     }
 }
@@ -57,9 +60,9 @@ impl PartialOrd for PartialVersion {
 impl fmt::Display for PartialVersion {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Self::A(a, b, c) => fmt.pad(&format!("{}.{}.{}", a, b, c)),
-            Self::B(a, b) => fmt.pad(&format!("{}.{}", a, b)),
-            Self::C(a) => fmt.pad(&format!("{}", a)),
+            Self::Mmp(a, b, c) => fmt.pad(&format!("{}.{}.{}", a, b, c)),
+            Self::Mm(a, b) => fmt.pad(&format!("{}.{}", a, b)),
+            Self::M(a) => fmt.pad(&format!("{}", a)),
         }
     }
 }
@@ -75,9 +78,9 @@ impl FromStr for PartialVersion {
                 caps.get(2).and_then(|n| n.as_str().parse::<u32>().ok()),
                 caps.get(3).and_then(|n| n.as_str().parse::<u32>().ok()),
             ) {
-                (Some(a), Some(b), Some(c)) => Ok(Self::A(a, b, c)),
-                (Some(a), Some(b), _) => Ok(Self::B(a, b)),
-                (Some(a), _, _) => Ok(Self::C(a)),
+                (Some(a), Some(b), Some(c)) => Ok(Self::Mmp(a, b, c)),
+                (Some(a), Some(b), _) => Ok(Self::Mm(a, b)),
+                (Some(a), _, _) => Ok(Self::M(a)),
                 _ => Err(VersionError::BadlyFormed),
             },
             None => Err(VersionError::Missing),
@@ -95,12 +98,12 @@ mod tests {
 
     #[test]
     fn parses_version_below_10() {
-        assert_eq!(Ok(A(9, 6, 17)), "9.6.17".parse());
+        assert_eq!(Ok(Mmp(9, 6, 17)), "9.6.17".parse());
     }
 
     #[test]
     fn parses_version_above_10() {
-        assert_eq!(Ok(B(12, 2)), "12.2".parse());
+        assert_eq!(Ok(Mm(12, 2)), "12.2".parse());
     }
 
     #[test]
@@ -116,26 +119,26 @@ mod tests {
 
     #[test]
     fn displays_version_below_10() {
-        assert_eq!("9.6.17", format!("{}", A(9, 6, 17)));
+        assert_eq!("9.6.17", format!("{}", Mmp(9, 6, 17)));
     }
 
     #[test]
     fn displays_version_above_10() {
-        assert_eq!("12.2", format!("{}", B(12, 2)));
+        assert_eq!("12.2", format!("{}", Mm(12, 2)));
     }
 
     #[test]
     fn partial_ord_works_as_expected() {
         let mut versions = vec![
-            A(9, 10, 11),
-            A(9, 10, 12),
-            B(8, 11),
-            B(9, 11),
-            B(9, 12),
-            B(10, 11),
-            C(8),
-            C(9),
-            C(11),
+            Mmp(9, 10, 11),
+            Mmp(9, 10, 12),
+            Mm(8, 11),
+            Mm(9, 11),
+            Mm(9, 12),
+            Mm(10, 11),
+            M(8),
+            M(9),
+            M(11),
         ];
         let mut rng = thread_rng();
         for _ in 0..1000 {
@@ -144,15 +147,15 @@ mod tests {
             assert_eq!(
                 versions,
                 vec![
-                    C(8),
-                    B(8, 11),
-                    C(9),
-                    A(9, 10, 11),
-                    A(9, 10, 12),
-                    B(9, 11),
-                    B(9, 12),
-                    B(10, 11),
-                    C(11),
+                    M(8),
+                    Mm(8, 11),
+                    M(9),
+                    Mmp(9, 10, 11),
+                    Mmp(9, 10, 12),
+                    Mm(9, 11),
+                    Mm(9, 12),
+                    Mm(10, 11),
+                    M(11),
                 ]
             );
         }
@@ -161,17 +164,17 @@ mod tests {
     #[test]
     fn sort_key_works_as_expected() {
         let mut versions = vec![
-            A(9, 0, 0),
-            A(9, 10, 11),
-            A(9, 10, 12),
-            B(9, 0),
-            B(8, 11),
-            B(9, 11),
-            B(9, 12),
-            B(10, 11),
-            C(8),
-            C(9),
-            C(11),
+            Mmp(9, 0, 0),
+            Mmp(9, 10, 11),
+            Mmp(9, 10, 12),
+            Mm(9, 0),
+            Mm(8, 11),
+            Mm(9, 11),
+            Mm(9, 12),
+            Mm(10, 11),
+            M(8),
+            M(9),
+            M(11),
         ];
         let mut rng = thread_rng();
         for _ in 0..1000 {
@@ -180,17 +183,17 @@ mod tests {
             assert_eq!(
                 versions,
                 vec![
-                    C(8),
-                    B(8, 11),
-                    C(9),
-                    B(9, 0),
-                    A(9, 0, 0),
-                    A(9, 10, 11),
-                    A(9, 10, 12),
-                    B(9, 11),
-                    B(9, 12),
-                    B(10, 11),
-                    C(11),
+                    M(8),
+                    Mm(8, 11),
+                    M(9),
+                    Mm(9, 0),
+                    Mmp(9, 0, 0),
+                    Mmp(9, 10, 11),
+                    Mmp(9, 10, 12),
+                    Mm(9, 11),
+                    Mm(9, 12),
+                    Mm(10, 11),
+                    M(11),
                 ]
             );
         }
