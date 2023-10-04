5d328e32272d8abae839962c74f2802c90d6dce6
Gavin Panella <gavin@allenap.me>
2023-10-04 17:23:03 +0200

    Change `PartialVersion` to a similar form as `Version`

--- src/version/partial.rs
+++ src/version/partial.rs
@@ -8,12 +8,28 @@ use super::{Version, VersionError};
 
 #[derive(Copy, Clone, Debug)]
 pub enum PartialVersion {
-    /// Major, Minor, Patch.
-    Mmp(u32, u32, u32),
-    /// Major, Minor.
-    Mm(u32, u32),
-    /// Major.
-    M(u32),
+    Pre10m(u32, u32),
+    Pre10mm(u32, u32, u32),
+    Post10m(u32),
+    Post10mm(u32, u32),
+}
+
+/// Convert a [`PartialVersion`] into a [`Version`] that's useful for
+/// comparisons.
+///
+/// The [`Version`] returned has 0 (zero) in the place of the missing parts. For
+/// example, a partial version of `9.6.*` becomes `9.6.0`, and `12.*` becomes
+/// `12.0`.
+impl From<&PartialVersion> for Version {
+    fn from(partial: &PartialVersion) -> Self {
+        use PartialVersion::*;
+        match *partial {
+            Pre10m(a, b) => Version::Pre10(a, b, 0),
+            Pre10mm(a, b, c) => Version::Pre10(a, b, c),
+            Post10m(a) => Version::Post10(a, 0),
+            Post10mm(a, b) => Version::Post10(a, b),
+        }
+    }
 }
 
 impl PartialVersion {
@@ -22,35 +38,30 @@ impl PartialVersion {
     /// Put another way: can a server of the given [`Version`] be used to run a
     /// cluster of this [`PartialVersion`]?
     ///
-    /// This is an interesting question to answer, because clusters contain a
-    /// file named `PG_VERSION` which containing the major version number of the
-    /// cluster's files, e.g. "15" or "9.6".
+    /// This is an interesting question to answer because clusters contain a
+    /// file named `PG_VERSION` which containing just the major version
+    /// number/numbers of the cluster's files, e.g. "15" or "9.6".
     ///
     /// For versions of PostgreSQL before 10, this means that the given
-    /// version's major and minor numbers must match exactly, and the patch
-    /// number must be greater than or equal to this `PartialVersion`'s patch
-    /// number. When this `PartialVersion` has no minor or patch number, the
-    /// given version is assumed to be compatible.
+    /// version's major numbers must match exactly, and the patch number must be
+    /// greater than or equal to this `PartialVersion`'s patch number. When this
+    /// `PartialVersion` has no minor or patch number, the given version is
+    /// assumed to be compatible.
     ///
     /// For versions of PostgreSQL after and including 10, this means that the
     /// given version's major number must match exactly, and the minor number
     /// must be greater than or equal to this `PartialVersion`'s minor number.
     /// When this `PartialVersion` has no minor number, the given version is
-    /// assumed to be compatible. The patch number is ignored.
+    /// assumed to be compatible.
     #[allow(dead_code)]
     pub fn compatible(&self, version: Version) -> bool {
         use PartialVersion::*;
-        match version {
-            Version::Pre10(va, vb, vc) => match *self {
-                Mmp(a, b, c) => a == va && b == vb && c <= vc,
-                Mm(a, b) => a == va && b == vb,
-                M(a) => a == va,
-            },
-            Version::Post10(va, vb) => match *self {
-                Mmp(a, b, _) => a == va && b <= vb,
-                Mm(a, b) => a == va && b <= vb,
-                M(a) => a == va,
-            },
+        match (*self, version) {
+            (Pre10m(a, b), Version::Pre10(x, y, _)) => a == x && b == y,
+            (Pre10mm(a, b, c), Version::Pre10(x, y, z)) => a == x && b == y && c <= z,
+            (Post10m(a), Version::Post10(x, _)) => a == x,
+            (Post10mm(a, b), Version::Post10(x, y)) => a == x && b <= y,
+            _ => false,
         }
     }
 
@@ -64,9 +75,10 @@ impl PartialVersion {
     pub fn sort_key(&self) -> (u32, Option<u32>, Option<u32>) {
         use PartialVersion::*;
         match *self {
-            Mmp(a, b, c) => (a, Some(b), Some(c)),
-            Mm(a, b) => (a, Some(b), None),
-            M(a) => (a, None, None),
+            Pre10m(a, b) => (a, Some(b), None),
+            Pre10mm(a, b, c) => (a, Some(b), Some(c)),
+            Post10m(a) => (a, None, None),
+            Post10mm(a, b) => (a, Some(b), None),
         }
     }
 }
@@ -81,25 +93,36 @@ impl PartialOrd for PartialVersion {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         use PartialVersion::*;
         match (*self, *other) {
-            (Mmp(x1, x2, x3), Mmp(y1, y2, y3)) => (x1, x2, x3).partial_cmp(&(y1, y2, y3)),
-            (Mmp(x1, x2, x3), Mm(y1, y2)) => (x1, x2, x3).partial_cmp(&(y1, y2, 0)),
-            (Mmp(x1, x2, x3), M(y1)) => (x1, x2, x3).partial_cmp(&(y1, 0, 0)),
-            (Mm(x1, x2), Mmp(y1, y2, y3)) => (x1, x2, 0).partial_cmp(&(y1, y2, y3)),
-            (Mm(x1, x2), Mm(y1, y2)) => (x1, x2).partial_cmp(&(y1, y2)),
-            (Mm(x1, x2), M(y1)) => (x1, x2).partial_cmp(&(y1, 0)),
-            (M(x1), Mmp(y1, y2, y3)) => (x1, 0, 0).partial_cmp(&(y1, y2, y3)),
-            (M(x1), Mm(y1, y2)) => (x1, 0).partial_cmp(&(y1, y2)),
-            (M(x1), M(y1)) => x1.partial_cmp(&y1),
+            (Pre10m(a, b), Pre10m(x, y)) => Some((a, b).cmp(&(x, y))),
+            (Pre10m(a, b), Pre10mm(x, y, _)) => Some((a, b).cmp(&(x, y))),
+            (Pre10mm(a, b, _), Pre10m(x, y)) => Some((a, b).cmp(&(x, y))),
+            (Pre10mm(a, b, c), Pre10mm(x, y, z)) => Some((a, b, c).cmp(&(x, y, z))),
+
+            (Post10m(a), Post10m(x)) => Some(a.cmp(&x)),
+            (Post10m(a), Post10mm(x, _)) => Some(a.cmp(&x)),
+            (Post10mm(a, _), Post10m(x)) => Some(a.cmp(&x)),
+            (Post10mm(a, b), Post10mm(x, y)) => Some((a, b).cmp(&(x, y))),
+
+            (Pre10m(..), Post10m(..)) => Some(Ordering::Less),
+            (Pre10m(..), Post10mm(..)) => Some(Ordering::Less),
+            (Pre10mm(..), Post10m(..)) => Some(Ordering::Less),
+            (Pre10mm(..), Post10mm(..)) => Some(Ordering::Less),
+
+            (Post10m(..), Pre10m(..)) => Some(Ordering::Greater),
+            (Post10m(..), Pre10mm(..)) => Some(Ordering::Greater),
+            (Post10mm(..), Pre10m(..)) => Some(Ordering::Greater),
+            (Post10mm(..), Pre10mm(..)) => Some(Ordering::Greater),
         }
     }
 }
 
 impl fmt::Display for PartialVersion {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Self::Mmp(a, b, c) => fmt.pad(&format!("{}.{}.{}", a, b, c)),
-            Self::Mm(a, b) => fmt.pad(&format!("{}.{}", a, b)),
-            Self::M(a) => fmt.pad(&format!("{}", a)),
+        match *self {
+            Self::Pre10m(a, b) => fmt.pad(&format!("{a}.{b}.*")),
+            Self::Pre10mm(a, b, c) => fmt.pad(&format!("{a}.{b}.{c}")),
+            Self::Post10m(a) => fmt.pad(&format!("{a}.*")),
+            Self::Post10mm(a, b) => fmt.pad(&format!("{a}.{b}")),
         }
     }
 }
@@ -115,9 +138,10 @@ impl FromStr for PartialVersion {
                 caps.get(2).and_then(|n| n.as_str().parse::<u32>().ok()),
                 caps.get(3).and_then(|n| n.as_str().parse::<u32>().ok()),
             ) {
-                (Some(a), Some(b), Some(c)) => Ok(Self::Mmp(a, b, c)),
-                (Some(a), Some(b), _) => Ok(Self::Mm(a, b)),
-                (Some(a), _, _) => Ok(Self::M(a)),
+                (Some(a), Some(b), None) if a < 10 => Ok(Self::Pre10m(a, b)),
+                (Some(a), Some(b), Some(c)) if a < 10 => Ok(Self::Pre10mm(a, b, c)),
+                (Some(a), None, None) if a >= 10 => Ok(Self::Post10m(a)),
+                (Some(a), Some(b), None) if a >= 10 => Ok(Self::Post10mm(a, b)),
                 _ => Err(VersionError::BadlyFormed),
             },
             None => Err(VersionError::Missing),
@@ -135,18 +159,24 @@ mod tests {
 
     #[test]
     fn parses_version_below_10() {
-        assert_eq!(Ok(Mmp(9, 6, 17)), "9.6.17".parse());
+        assert_eq!(Ok(Pre10mm(9, 6, 17)), "9.6.17".parse());
+        assert_eq!(Ok(Pre10m(9, 6)), "9.6".parse());
     }
 
     #[test]
     fn parses_version_above_10() {
-        assert_eq!(Ok(Mm(12, 2)), "12.2".parse());
+        assert_eq!(Ok(Post10mm(12, 2)), "12.2".parse());
+        assert_eq!(Ok(Post10m(12)), "12".parse());
     }
 
     #[test]
     fn parse_returns_error_when_version_is_invalid() {
         // 4294967295 is (2^32 + 1), so won't fit in a u32.
         assert_eq!(Err(BadlyFormed), "4294967296.0".parse::<PartialVersion>());
+        // Before version 10, there are always at least two parts in a version.
+        assert_eq!(Err(BadlyFormed), "9".parse::<PartialVersion>());
+        // From version 10 onwards, there are only two parts in a version.
+        assert_eq!(Err(BadlyFormed), "10.10.10".parse::<PartialVersion>());
     }
 
     #[test]
@@ -156,54 +186,68 @@ mod tests {
 
     #[test]
     fn displays_version_below_10() {
-        assert_eq!("9.6.17", format!("{}", Mmp(9, 6, 17)));
+        assert_eq!("9.6.17", format!("{}", Pre10mm(9, 6, 17)));
+        assert_eq!("9.6.*", format!("{}", Pre10m(9, 6)));
     }
 
     #[test]
     fn displays_version_above_10() {
-        assert_eq!("12.2", format!("{}", Mm(12, 2)));
+        assert_eq!("12.2", format!("{}", Post10mm(12, 2)));
+        assert_eq!("12.*", format!("{}", Post10m(12)));
     }
 
     #[test]
     fn compatible_below_10() {
-        assert!(Mmp(9, 6, 17).compatible("9.6.17".parse().unwrap()));
-        assert!(Mm(9, 6).compatible("9.6.17".parse().unwrap()));
-        assert!(M(9).compatible("9.6.17".parse().unwrap()));
+        let version = "9.6.16".parse().unwrap();
+        assert!(Pre10mm(9, 6, 16).compatible(version));
+        assert!(Pre10m(9, 6).compatible(version));
     }
 
     #[test]
     fn not_compatible_below_10() {
-        assert!(!Mmp(9, 6, 17).compatible("9.6.16".parse().unwrap()));
-        assert!(!Mm(9, 6).compatible("9.5.17".parse().unwrap()));
-        assert!(!M(9).compatible("8.6.17".parse().unwrap()));
+        let version = "9.6.16".parse().unwrap();
+        assert!(!Pre10mm(9, 6, 17).compatible(version));
+        assert!(!Pre10m(9, 7).compatible(version));
+        assert!(!Pre10mm(8, 6, 16).compatible(version));
+        assert!(!Pre10m(8, 6).compatible(version));
     }
 
     #[test]
     fn compatible_above_10() {
-        assert!(Mmp(12, 6, 1234).compatible("12.6".parse().unwrap()));
-        assert!(Mm(13, 2).compatible("13.2".parse().unwrap()));
-        assert!(M(14).compatible("14.7".parse().unwrap()));
+        let version = "12.6".parse().unwrap();
+        assert!(Post10mm(12, 6).compatible(version));
+        assert!(Post10m(12).compatible(version));
     }
 
     #[test]
     fn not_compatible_above_10() {
-        assert!(!Mmp(12, 6, 1234).compatible("12.5".parse().unwrap()));
-        assert!(!Mm(13, 2).compatible("13.1".parse().unwrap()));
-        assert!(!M(14).compatible("13.7".parse().unwrap()));
+        let version = "12.6".parse().unwrap();
+        assert!(!Post10mm(12, 7).compatible(version));
+        assert!(!Post10m(13).compatible(version));
+        assert!(!Post10mm(11, 6).compatible(version));
+        assert!(!Post10m(11).compatible(version));
+    }
+
+    #[test]
+    fn not_compatible_below_10_with_above_10() {
+        let version = "12.6".parse().unwrap();
+        assert!(!Pre10m(9, 1).compatible(version));
+        assert!(!Pre10mm(9, 1, 2).compatible(version));
+        let version = "9.1.2".parse().unwrap();
+        assert!(!Post10m(12).compatible(version));
+        assert!(!Post10mm(12, 6).compatible(version));
     }
 
     #[test]
     fn partial_ord_works_as_expected() {
         let mut versions = vec![
-            Mmp(9, 10, 11),
-            Mmp(9, 10, 12),
-            Mm(8, 11),
-            Mm(9, 11),
-            Mm(9, 12),
-            Mm(10, 11),
-            M(8),
-            M(9),
-            M(11),
+            Pre10mm(9, 10, 11),
+            Pre10mm(9, 10, 12),
+            Pre10m(8, 11),
+            Pre10m(9, 11),
+            Pre10m(9, 12),
+            Post10mm(10, 11),
+            Post10m(11),
         ];
         let mut rng = thread_rng();
         for _ in 0..1000 {
@@ -212,15 +256,13 @@ mod tests {
             assert_eq!(
                 versions,
                 vec![
-                    M(8),
-                    Mm(8, 11),
-                    M(9),
-                    Mmp(9, 10, 11),
-                    Mmp(9, 10, 12),
-                    Mm(9, 11),
-                    Mm(9, 12),
-                    Mm(10, 11),
-                    M(11),
+                    Pre10m(8, 11),
+                    Pre10mm(9, 10, 11),
+                    Pre10mm(9, 10, 12),
+                    Pre10m(9, 11),
+                    Pre10m(9, 12),
+                    Post10mm(10, 11),
+                    Post10m(11),
                 ]
             );
         }
@@ -229,17 +271,15 @@ mod tests {
     #[test]
     fn sort_key_works_as_expected() {
         let mut versions = vec![
-            Mmp(9, 0, 0),
-            Mmp(9, 10, 11),
-            Mmp(9, 10, 12),
-            Mm(9, 0),
-            Mm(8, 11),
-            Mm(9, 11),
-            Mm(9, 12),
-            Mm(10, 11),
-            M(8),
-            M(9),
-            M(11),
+            Pre10mm(9, 0, 0),
+            Pre10mm(9, 10, 11),
+            Pre10mm(9, 10, 12),
+            Pre10m(9, 0),
+            Pre10m(8, 11),
+            Pre10m(9, 11),
+            Pre10m(9, 12),
+            Post10mm(10, 11),
+            Post10m(11),
         ];
         let mut rng = thread_rng();
         for _ in 0..1000 {
@@ -248,17 +288,15 @@ mod tests {
             assert_eq!(
                 versions,
                 vec![
-                    M(8),
-                    Mm(8, 11),
-                    M(9),
-                    Mm(9, 0),
-                    Mmp(9, 0, 0),
-                    Mmp(9, 10, 11),
-                    Mmp(9, 10, 12),
-                    Mm(9, 11),
-                    Mm(9, 12),
-                    Mm(10, 11),
-                    M(11),
+                    Pre10m(8, 11),
+                    Pre10m(9, 0),
+                    Pre10mm(9, 0, 0),
+                    Pre10mm(9, 10, 11),
+                    Pre10mm(9, 10, 12),
+                    Pre10m(9, 11),
+                    Pre10m(9, 12),
+                    Post10mm(10, 11),
+                    Post10m(11),
                 ]
             );
         }
